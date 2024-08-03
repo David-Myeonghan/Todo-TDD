62e5504b15a17b8e4a51a2e346d98efaea3343f4
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-08-04 01:33:07 +0900

    feat: 기본 스타일링 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import {TodoContextProvider} from './TodoContext';
+import Input from './Input';
+import TodoItemList from "./TodoItemList";
 
 function App() {
-  
-  return (
-    <div className="App">
-      <input placeholder='What needs to be done?' />
-    </div>
-  );
+
+    return (
+        <div className="app">
+            <TodoContextProvider>
+                <div className='wrapper'>
+                    <Input/>
+                    <TodoItemList/>
+                </div>
+
+            </TodoContextProvider>
+        </div>
+    );
 }
 
 export default App;
