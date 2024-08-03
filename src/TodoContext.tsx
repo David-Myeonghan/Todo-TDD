b6e8531a624d8ac9d3aedcd07f2a25d1579314bd
David Myeonghan Ryu <myeonghan12@gmail.com>
2024-08-03 16:56:10 +0900

    fix: context 수정

--- src/TodoContext.tsx
+++ src/TodoContext.tsx
@@ -1,27 +1,25 @@
-import { createContext, useState, useMemo, useEffect } from "react"
-import { getLocalStorageItem } from "./localStorage";
+import React, {createContext, SetStateAction, useEffect, useMemo, useState} from "react"
+import {getLocalStorageItem} from "./localStorage";
 
 type TodoItem = {
     title: string;
     isDone: boolean;
 }
 
-interface TodoList {
-    todoList: Array<TodoItem>;
-}
+type TodoList = Array<TodoItem>
 
 type TodoContextType = {
     todoList: TodoList,
+    setTodoList: React.Dispatch<SetStateAction<TodoList>>,
 }
-
 
 export const TodoContext = createContext<TodoContextType | []>([]);
 
-export const TodoContextProvider = ({children}: {children: React.ReactNode}) => {
+export const TodoContextProvider = ({children}: { children: React.ReactNode }) => {
     const [todoList, setTodoList] = useState<TodoList>([]);
 
     useEffect(() => {
-        const storageTodos = getLocalStorageItem('todos'); 
+        const storageTodos = getLocalStorageItem('todos');
         setTodoList(storageTodos)
 
     }, [])
